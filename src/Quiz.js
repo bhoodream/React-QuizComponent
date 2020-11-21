114d45410e6ff52480bab0b1e32cc124fc0930cd
Fedorov Vadim <bhoodream@yandex.ru>
2020-11-21 16:31:15 +0700

    module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,8 +14,13 @@ class Quiz extends Component {
         };
     }
 
+    showNextQuestion() {
+        this.setState({ quiz_position: this.state.quiz_position + 1 });
+    }
+
     render() {
-        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+        const isQuizEnd =
+            this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
         return (
             <div>
@@ -23,6 +28,9 @@ class Quiz extends Component {
                     <QuizEnd />
                 ) : (
                     <QuizQuestion
+                        showNextQuestionHandler={this.showNextQuestion.bind(
+                            this
+                        )}
                         quiz_question={
                             quizData.quiz_questions[
                                 this.state.quiz_position - 1
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    handleClick(buttonText) {
+        buttonText === this.props.quiz_question.answer &&
+            this.props.showNextQuestionHandler();
+    }
+
     render() {
         return (
             <main>
@@ -11,13 +16,15 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>
-                            <QuizQuestionButton
-                                button_text={
-                                    this.props.quiz_question.answer_options[0]
-                                }
-                            />
-                        </li>
+                        {this.props.quiz_question.answer_options.map(
+                            (answer_option, index) => (
+                                <QuizQuestionButton
+                                    clickHandler={this.handleClick.bind(this)}
+                                    key={index}
+                                    button_text={answer_option}
+                                />
+                            )
+                        )}
                     </ul>
                 </section>
             </main>
